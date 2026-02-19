58ad945725e3ab0fcaf2f712300546ec582478d1
bddap (opencode opus-4.6) <andrew+bot@dirksen.com>
2026-02-19 00:45:17 +0000

    Add molecular formula and weight calculations

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod atom;
 pub mod bond;
 pub mod element;
+pub mod formula;
 pub mod mol;
 pub mod smiles;
 pub mod traits;
@@ -9,6 +10,7 @@ pub mod wrappers;
 pub use atom::{Atom, Chirality};
 pub use bond::{Bond, BondOrder, BondStereo, SmilesBond, SmilesBondOrder};
 pub use element::Element;
+pub use formula::{average_mol_weight, exact_mol_weight, mol_formula};
 pub use mol::Mol;
 pub use smiles::{parse_smiles, SmilesError};
 pub use traits::{
